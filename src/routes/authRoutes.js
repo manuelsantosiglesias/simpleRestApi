f3258845cabe57f2f6dc64a77d4afc75365a436b
Santos <nomail@mail.es>
2025-02-10 16:27:09 +0100

    Añadida ruta refresh token

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { login, logout } from '../controllers/authController.js';
+import { login, logout, refreshToken } from '../controllers/authController.js';
 import { register } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/logout', logout);
+router.post('/refresh', refreshToken);
 router.post('/register', register);
 
 export default router;
