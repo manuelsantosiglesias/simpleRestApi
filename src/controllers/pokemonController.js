4a75031ea5414b2f4c96ef1f91a26237b793087d
Santos <nomail@mail.es>
2025-02-12 17:22:49 +0100

    Número mágico a constante de clase

--- src/controllers/pokemonController.js
+++ src/controllers/pokemonController.js
@@ -1,10 +1,12 @@
 import axios from '../utils/axiosInstance.js';
 import emailService from '../services/emailService.js';
 
+const POKEMON_API_URL = 'https://pokeapi.co/api/v2/pokemon?limit=151';
+
 export const getPokemon = async (req, res) => {
     try {
         // TODO: PRUEBA DE CONCEPTO PASAR A HANDLER
-        const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151');
+        const response = await axios.get(POKEMON_API_URL);
         res.json(response.data);
     } catch (error) {
         // MAIL SI NO ES POSIBLE OBTENER LOS DATOS
