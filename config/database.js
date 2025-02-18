fc9b14d40f55d7dd7f1fd5d3d24a375e4144a084
Santos <nomail@mail.es>
2025-02-18 10:45:35 +0100

    fix config path

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize';
-import '../config/config.js';
+import '../src/config/config.js';
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
